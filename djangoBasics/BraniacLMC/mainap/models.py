3e25ea96926c2406f6374b2e13c7ab923f08f502
Pavel Sadovnikov <deimos007@rambler.ru>
2022-05-25 20:48:49 +0200

    Models, fixtures draft

--- djangoBasics/BraniacLMC/mainap/models.py
+++ djangoBasics/BraniacLMC/mainap/models.py
@@ -1,3 +1,73 @@
 from django.db import models
 
-# Create your models here.
+NULLALLOW = {'blank': True, 'null': True}
+# body = models.TextField(blank=True, null=True, verbose_name="Body")
+
+
+class BaseModel(models.Model):
+    def delete(self, *args):
+        self.deleted = True
+        self.save()
+
+
+class News(BaseModel):
+    title = models.CharField(max_length=256, verbose_name="Title")
+    preamble = models.CharField(max_length=1024, verbose_name="Preamble")
+    body = models.TextField(**NULLALLOW, verbose_name="Body")
+    body_as_markdown = models.BooleanField(default=False, verbose_name="As markdown")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Created", editable=False)
+    updated = models.DateTimeField(auto_now=True, verbose_name="Edited", editable=False)
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f"{self.pk} {self.title}"
+
+    class Meta:
+        ordering = ("created", "title")
+
+
+class Courses(BaseModel):
+    name = models.CharField(max_length=256, verbose_name="Name")
+    description = models.TextField(verbose_name="Description", blank=True, null=True)
+    description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
+    cost = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Cost", default=0)
+    cover = models.CharField(max_length=25, default="no_image.svg", verbose_name="Cover")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Created")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Edited")
+
+    def __str__(self) -> str:
+        return f"{self.pk} {self.name}"
+
+    class Meta:
+        ordering = ("name", "created")
+
+
+class Lesson(BaseModel):
+    course = models.ForeignKey(Courses, on_delete=models.CASCADE)
+    num = models.PositiveIntegerField(verbose_name="Lesson number")
+    title = models.CharField(max_length=256, verbose_name="Name")
+    description = models.TextField(verbose_name="Description", **NULLALLOW)
+    description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Created", editable=False)
+    updated = models.DateTimeField(auto_now=True, verbose_name="Edited", editable=False)
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f"{self.course.name} | {self.num} | {self.title}"
+
+    class Meta:
+        ordering = ("course", "num")
+
+
+class CourseTeachers(BaseModel):
+    course = models.ManyToManyField(Courses)
+    name_first = models.CharField(max_length=128, verbose_name="Name")
+    name_second = models.CharField(max_length=128, verbose_name="Surname")
+    day_birth = models.DateField(verbose_name="Birth date")
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f"{self.pk}, {self.name_second}, {self.name_first}"
+
+    class Meta:
+        ordering = ("name_second", "name_first")
